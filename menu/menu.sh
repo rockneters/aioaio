82e62fd7e30c701fdfe6d6626074949614de6041
rockneters <89226199+rockneters@users.noreply.github.com>
2021-10-12 09:27:37 +0700

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,219 +1,247 @@
 #!/bin/bash
+#Licensed to https://www.hostingtermurah.net/
+clear
+if [[ -e /etc/debian_version ]]; then
+	OS=debian
+	RCLOCAL='/etc/rc.local'
+elif [[ -e /etc/centos-release || -e /etc/redhat-release ]]; then
+	OS=centos
+	RCLOCAL='/etc/rc.d/rc.local'
+	chmod +x /etc/rc.d/rc.local
+else
+	echo "It looks like you are not running this installer on Debian, Ubuntu or Centos system"
+	exit
+fi
 color1='\e[031;1m'
 color2='\e[34;1m'
 color3='\e[0m'
-domain="$(cat /etc/v2ray/domain)"
-ip="$(grep -oP '(?<="ip": ")[^"]*' /etc/datadiri.conf)"
-isp="$(grep -oP '(?<="org": ")[^"]*' /etc/datadiri.conf)"
-city="$(grep -oP '(?<="city": ")[^"]*' /etc/datadiri.conf)"
-region="$(grep -oP '(?<="region": ")[^"]*' /etc/datadiri.conf)"
-country="$(grep -oP '(?<="country": ")[^"]*' /etc/datadiri.conf)"
-loc="$(grep -oP '(?<="loc": ")[^"]*' /etc/datadiri.conf)"
-timezone="$(grep -oP '(?<="timezone": ")[^"]*' /etc/datadiri.conf)"
+echo -e "==================================================="
+echo -e "-------- Welcome To Premium Script Menu -----------"
+	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
+	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
+	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
+	tram=$( free -m | awk 'NR==2 {print $2}' )
+	swap=$( free -m | awk 'NR==4 {print $2}' )
+	up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
 
-cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-tram=$( free -m | awk 'NR==2 {print $2}' )
-swap=$( free -m | awk 'NR==4 {print $2}' )
-up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-clear
-echo "----------------- Welcome To Premium Script Menu -----------------"
 echo -e "\e[032;1mCPU Model:\e[0m $cname"
 echo -e "\e[032;1mNumber Of Cores:\e[0m $cores"
 echo -e "\e[032;1mCPU Frequency:\e[0m $freq MHz"
 echo -e "\e[032;1mTotal Amount Of RAM:\e[0m $tram MB"
 echo -e "\e[032;1mTotal Amount Of Swap:\e[0m $swap MB"
 echo -e "\e[032;1mSystem Uptime:\e[0m $up"
-echo -e "---------------------------------------------------------------"
-echo -e ">>_+============= Rocknet VPN Premium Script ==============+_<<"
-echo -e "[-]Domain/Host    : $domain"
-echo -e "[-]IP Address     : $ip"
-echo -e "[-]ISP            : $isp"
-echo -e "[-]City           : $city"
-echo -e "[-]Region         : $region"
-echo -e "[-]Country        : $country"
-echo -e "[-]Location       : $loc"
-echo -e "[-]Timezone       : $timezone"
-echo -e ">>_+====================== SSH Menu =====================+_<<"
-echo -e "[1][addssh]       : Menambah User SSH"
-echo -e "[2][delssh]       : Menghapus User SSH"
-echo -e "[3][expssh]       : Menghapus User SSH yang Expired"
-echo -e "[4][cek]          : Melihat Login User SSH"
-echo -e "[5][listssh]      : Melihat Daftar User SSH"
-echo -e ">>_+===================== V2Ray Menu ====================+_<<"
-echo -e "[6][addv2ray]     : Menambah User V2Ray"
-echo -e "[7][delv2ray]     : Menghapus User V2Ray"
-echo -e "[8][expv2ray]     : Menghapus User V2Ray yang Expired"
-echo -e "[9][listv2ray]    : Melihat Daftar User V2Ray"
-echo -e ">>_+====================== SSR Menu =====================+_<<"
-echo -e "[10][addssr]      : Menambah User SSR"
-echo -e "[11][delssr]      : Menghapus User SSR"
-echo -e "[12][expssr]      : Menghapus User SSR yang Expired"
-echo -e "[13][listssr]     : Melihat Daftar User SSR"
-echo -e ">>_+==================== Trojan Menu ====================+_<<"
-echo -e "[14][addtrojan]   : Menambah User Trojan"
-echo -e "[15][deltrojan]   : Menghapus User Trojan"
-echo -e "[16][exptrojan]   : Menghapus User Trojan yang Expired"
-echo -e "[17][listtrojan]  : Melihat Daftar User Trojan"
-echo -e ">>_+=================== Wireguard Menu ==================+_<<"
-echo -e "[30][addwg]       : Menambah User Wireguard"
-echo -e "[31][delwg]       : Menghapus User Wireguard"
-echo -e "[32][expwg]       : Menghapus User Wireguard yang Expired"
-echo -e "[33][listwg]      : Melihat Daftar User Wireguard"
-echo -e ">>_+================== Shadowsocks Menu =================+_<<"
-echo -e "[34][addss]       : Menambah User Shadowsocks"
-echo -e "[35][delss]       : Menghapus User Shadowsocks"
-echo -e "[36][expss]       : Menghapus User Shadowsocks yang Expired"
-echo -e "[37][listss]      : Melihat Daftar User Shadowsocks"
-echo -e ">>_+==================== Utility Menu ===================+_<<"
-echo -e "[38][restart]     : Merestart Semua Service"
-echo -e "[39][usage]       : Melihat Pemakaian System"
-echo -e "[40][vnstat]      : Melihat Pemakaian Bandwith VPS"
-echo -e "[41][info]        : Menampilkan Informasi System"
-echo -e "[42][speedtest]   : Test Speed VPS Server"
-echo -e "[43][about]       : Menampilkan informasi script"
-echo -e "[44][reboot]      : Memulai Ulang VPS"
-echo -e "[0][exit]         : Exit Menu"
-echo -e "\n"
-read -p "Pilih Menu No [ 0 - 44 ] : " input
-
-if [ $input == 0 ]
-then
-	exit
-elif [ $input == 1 ]
-then
-	addssh
-elif [ $input == 2 ]
-then
-	delssh
-elif [ $input == 3 ]
-then
-	expssh
-elif [ $input == 4 ]
-then
-	cek
-elif [ $input == 5 ]
-then
-	listssh
-elif [ $input == 6 ]
-then
-	addv2ray
-elif [ $input == 7 ]
-then
-	delv2ray
-elif [ $input == 8 ]
-then
-	exp
-elif [ $input == 9 ]
-then
-	listv2ray
-elif [ $input == 10 ]
-then
-	addssr
-elif [ $input == 11 ]
-then
-	delssr
-elif [ $input == 12 ]
-then
-	expssr
-elif [ $input == 13 ]
-then
-	listssr
-elif [ $input == 14 ]
-then
-	addtrojan
-elif [ $input == 15 ]
-then
-	deltrojan
-elif [ $input == 16 ]
-then
-	exptrojan
-elif [ $input == 17 ]
-then
-	listtrojan
-elif [ $input == 18 ]
-then
-	addvless
-elif [ $input == 19 ]
-then
-	delvless
-elif [ $input == 20 ]
-then
-	expvless
-elif [ $input == 21 ]
-then
-	listvless
-elif [ $input == 22 ]
-then
-	addl2tp
-elif [ $input == 23 ]
-then
-	dell2tp
-elif [ $input == 24 ]
-then
-	expl2tp
-elif [ $input == 25 ]
-then
-	listl2tp
-elif [ $input == 26 ]
-then
-	addpptp
-elif [ $input == 27 ]
-then
-	delpptp
-elif [ $input == 28 ]
-then
-	exppptp
-elif [ $input == 29 ]
-then
-	listpptp
-elif [ $input == 30 ]
-then
-	addwg
-elif [ $input == 31 ]
-then
-	delwg
-elif [ $input == 32 ]
-then
-	expwg
-elif [ $input == 33 ]
-then
-	listwg
-elif [ $input == 34 ]
-then
-	addss
-elif [ $input == 35 ]
-then
-	delss
-elif [ $input == 36 ]
-then
-	expss
-elif [ $input == 37 ]
-then
-	listss
-elif [ $input == 38 ]
-then
-	restart
-elif [ $input == 39 ]
-then
-	usage
-elif [ $input == 40 ]
-then
-	vnstat
-elif [ $input == 41 ]
-then
-	neofetch
-elif [ $input == 42 ]
-then
-	speedtest
-elif [ $input == 43 ]
-then
-	about
-elif [ $input == 44 ]
-then
-	reboot
+echo -e "==================================================="
+echo -e "----------------[ SSH & OVPN OPTION ]--------------"
+echo -e "${color1}[01] • ${color3}Create User Account"
+echo -e "${color1}[02] • ${color3}Create Trial Account"
+echo -e "${color1}[03] • ${color3}Add Active Period SSH & OpenVPN Account"
+echo -e "${color1}[04] • ${color3}Delete User Account"
+echo -e "${color1}[05] • ${color3}Check Account Login"
+echo -e "${color1}[06] • ${color3}Daftar Member SSH & OpenVPN"
+echo -e "${color1}[07] • ${color3}Delete User Expired SSH & OpenVPN"
+echo -e "${color1}[08] • ${color3}Set up Autokill SSH"
+echo -e "${color1}[09] • ${color3}Displays Users Who Do Multi Login SSH"
+echo -e "${color1}[10] • ${color3}Restart Service Dropbear, Squid3, OpenVPN dan SSH"
+echo -e "==================================================="
+echo -e "---------------[ V2RAY/VMESS OPTION  ]-------------"
+echo -e "${color1}[11] • ${color3}Create V2ray/Vmess Account"
+echo -e "${color1}[12] • ${color3}Delete V2ray/Vmess Account"
+echo -e "${color1}[13] • ${color3}Extend V2ray/Vmess Account"
+echo -e "${color1}[14] • ${color3}Check User Login cek-ws"
+echo -e "==================================================="
+echo -e "----------------[ V2RAY/VLESS OPTION ]-------------"
+echo -e "${color1}[15] • ${color3}Create V2ray/Vless Account"
+echo -e "${color1}[16] • ${color3}Delete V2ray/Vless Account"
+echo -e "${color1}[17] • ${color3}Extend V2ray/Vless Account"
+echo -e "${color1}[18] • ${color3}Check User Login"
+echo -e "==================================================="
+echo -e "---------------[ TROJAN GFW  OPTION ]--------------"
+echo -e "${color1}[19] • ${color3}Create Trojan Account"
+echo -e "${color1}[20] • ${color3}Delete Trojan Account"
+echo -e "${color1}[21] • ${color3}Extend Trojan Account"
+echo -e "${color1}[22] • ${color3}Check User Login cek-tr"
+echo -e "==================================================="
+echo -e "------------[ SHADOWSOCKS OBSF OPTION ]------------"
+echo -e "${color1}[23] • ${color3}Create Shadowsocks Account"
+echo -e "${color1}[24] • ${color3}Delete Shadowsocks Account"
+echo -e "${color1}[25] • ${color3}Extend Shadowsocks Account"
+echo -e "${color1}[26] • ${color3}Check User Login"
+echo -e "==================================================="
+echo -e "---------------[SHADOWSOCKSR OPTION ]--------------"
+echo -e "${color1}[27] • ${color3}Create ShadowsocksR Account"
+echo -e "${color1}[28] • ${color3}Delete ShadowsocksR Account"
+echo -e "${color1}[29] • ${color3}Extend ShadowsocksR Account"
+echo -e "${color1}[30] • ${color3}Show Other SSR Menu"
+echo -e "==================================================="
+echo -e "--------------[  WIREGUARD  OPTION ]---------------"
+echo -e "${color1}[31] • ${color3}Create Wireguard Account"
+echo -e "${color1}[32] • ${color3}Delete Wireguard Account"
+echo -e "${color1}[33] • ${color3}Extend Wireguard Account"
+echo -e "${color1}[34] • ${color3}Check User Login"
+echo -e "${color1}[35] • ${color3}Check Wireguard User Interface"
+echo -e "==================================================="
+echo -e "------------------[  L2TP  OPTION ]----------------"
+echo -e "${color1}[36] • ${color3}Create L2TP Account"
+echo -e "${color1}[37] • ${color3}DeletE L2TP Account"
+echo -e "${color1}[38] • ${color3}Extend L2TP Account"
+echo -e "==================================================="
+echo -e "------------------[  PPTP  OPTION ]----------------"
+echo -e "${color1}[39] • ${color3}Create PPTP Account"
+echo -e "${color1}[40] • ${color3}Delete PPTP Account"
+echo -e "${color1}[41] • ${color3}Extend PPTP Account"
+echo -e "${color1}[42] • ${color3}Check User Login"
+echo -e "==================================================="
+echo -e "------------------[  SSTP  OPTION ]----------------"
+echo -e "${color1}[43] • ${color3}Create SSTP Account"
+echo -e "${color1}[44] • ${color3}Delete SSTP Account"
+echo -e "${color1}[45] • ${color3}Extend SSTP Account"
+echo -e "${color1}[46] • ${color3}Check User Login"
+echo -e "==================================================="
+echo -e "-----------------[ SYSTEM OPTION ]-----------------"
+echo -e "${color1}[47] • ${color3}Speedtest Server"
+echo -e "${color1}[48] • ${color3}Add Or Change Subdomain Host For VPS"
+echo -e "${color1}[49] • ${color3}Change Port Of Some Service"
+echo -e "${color1}[50] • ${color3}Autobackup Data VPS"
+echo -e "${color1}[51] • ${color3}Backup Data VPS"
+echo -e "${color1}[52] • ${color3}Restore Data VPS"
+echo -e "${color1}[53] • ${color3}Webmin Menu"
+echo -e "${color1}[54] • ${color3}Update To Latest Kernel"
+echo -e "${color1}[55] • ${color3}Limit Bandwith Speed Server"
+echo -e "${color1}[56] • ${color3}Check Usage of VPS Ram"
+echo -e "${color1}[57] • ${color3}Update To Latest Script Version"
+echo -e "${color1}[58] • ${color3}Reboot VPSreboot"
+echo -e "${color1}[59] • ${color3}Change Server Password"
+echo -e "${color1}[60] • ${color3}Renew Certificate V2ray"
+echo -e "${color1}[61] • ${color3}Exit Menu"
+echo "----------------Script Mod By Rocknet-----------------"
+read -p "Choose an option from (1-62): " x
+if test $x -eq 1|01; then
+usernew
+elif test $x -eq 2|02; then
+trial
+elif test $x -eq 3|03; then
+renew
+elif test $x -eq 4|04; then
+deluser
+elif test $x -eq 5|05; then
+cek
+elif test $x -eq 6|06; then
+member
+elif test $x -eq 7|07; then
+delete
+elif test $x -eq 8|08; then
+autokill
+elif test $x -eq 9|09; then
+ceklim
+elif test $x -eq 10; then
+restart
+elif test $x -eq 11; then
+add-v2ray
+elif test $x -eq 12; then
+del-ws
+elif test $x -eq 13; then
+renew-ws
+elif test $x -eq 14; then
+cek-ws
+elif test $x -eq 15; then
+add-vless
+elif test $x -eq 16; then
+del-vless
+elif test $x -eq 17; then
+renew-vless
+elif test $x -eq 18; then
+cek-ws
+elif test $x -eq 19; then
+add-tr
+elif test $x -eq 20; then
+del-tr
+elif test $x -eq 21; then
+renew-tr
+elif test $x -eq 22; then
+cek-tr
+elif test $x -eq 23; then
+add-ss
+elif test $x -eq 24; then
+del-ss
+elif test $x -eq 25; then
+renew-ss
+elif test $x -eq 26; then
+cek-ss
+elif test $x -eq 27; then
+add-ssr
+elif test $x -eq 28; then
+del-ssr
+elif test $x -eq 29; then
+renew-ssr
+elif test $x -eq 30; then
+ssr
+elif test $x -eq 31; then
+add-wg
+elif test $x -eq 32; then
+del-wg
+elif test $x -eq 33; then
+cek-wg
+elif test $x -eq 34; then
+renew-wg
+elif test $x -eq 35; then
+wg show
+elif test $x -eq 36; then
+add-l2tp
+elif test $x -eq 37; then
+del-l2tp
+elif test $x -eq 38; then
+renew-l2tp
+elif test $x -eq 39; then
+add-pptp
+elif test $x -eq 40; then
+del-pptp
+elif test $x -eq 41; then
+renew-pptp
+elif test $x -eq 42; then
+cek-pptp
+elif test $x -eq 43; then
+add-sstp
+elif test $x -eq 44; then
+del-sstp
+elif test $x -eq 45; then
+renew-sstp
+elif test $x -eq 46; then
+cek-sstp
+elif test $x -eq 47; then
+speedtest
+elif test $x -eq 48; then
+add-host
+elif test $x -eq 49; then
+change-port
+elif test $x -eq 50; then
+autobackup
+elif test $x -eq 51; then
+backup
+elif test $x -eq 52; then
+restore
+elif test $x -eq 53; then
+wbmn
+elif test $x -eq 54; then
+kernel-updt
+elif test $x -eq 55; then
+limit-speed
+elif test $x -eq 56; then
+ram
+elif test $x -eq 57; then
+update
+elif test $x -eq 58; then
+reboot
+elif test $x -eq 59; then
+passwd
+elif test $x -eq 60; then
+cert2vray
+elif test $x -eq 61; then
+echo " "
+echo "Goodbye Menu!"
+echo " "
+exit
 else
-  echo -e "Maaf menu yang dituju salah, silahkan ulangi"
-  menu
+echo "Options Not Available In Menu."
+echo " "
+exit
 fi
