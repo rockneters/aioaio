b9553d55bec68fa7119d31c5706bfe8954bc1d23
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-01 15:43:13 +0700

    Update cert.sh

--- menu/cert.sh
+++ menu/cert.sh
@@ -4,18 +4,18 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-clear
-echo start
+sleep 1
+#echo start
 sleep 0.5
 #source /var/lib/premium-script/ipvps.conf
-domain=cat /etc/v2ray/domain
+chmod +x /etc/v2ray/domain
+domain=$(cat /etc/v2ray/domain);
 systemctl stop v2ray
 systemctl stop v2ray@none
+clear
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 systemctl start v2ray
 systemctl start v2ray@none
 echo Done
-sleep 0.5
-clear 
-neofetch
+sleep 3
